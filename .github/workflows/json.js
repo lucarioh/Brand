47f07ae785f413227306fa5d282349dcb765a04f
lucarioh <46325549+lucarioh@users.noreply.github.com>
2019-11-18 10:42:32 -0300

    Create json.js

---
+++ .github/workflows/json.js
@@ -0,0 +1,138 @@
+	function ac1(){
+	document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+	
+	document.getElementById('c1').style.transition = '1s linear';
+	document.getElementById('c2').style.transition = '1s linear';
+	document.getElementById('c3').style.transition = '1s linear';
+
+	document.getElementById('c1').style.marginTop = '-637px';
+	document.getElementById('c5').style.marginTop = '0';
+	document.getElementById('c1').style.opacity = '1';
+
+	document.getElementById('c1').style.opacity = '0.5';	
+ }
+	function ac2(){
+	document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+	document.getElementById('c1').style.transition = '1s linear';
+	document.getElementById('c2').style.transition = '1s linear';
+	document.getElementById('c3').style.transition = '1s linear';
+	document.getElementById('c4').style.transition = '1s linear';
+	document.getElementById('c5').style.transition = '1s linear';
+
+	document.getElementById('c2').style.marginTop = '-637px';
+	document.getElementById('c2').style.opacity = '0.5';
+ }
+	function ac3(){
+	document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+	document.getElementById('c1').style.transition = '1s linear';
+	document.getElementById('c2').style.transition = '1s linear';
+	document.getElementById('c3').style.transition = '1s linear';
+	document.getElementById('c4').style.transition = '1s linear';
+	document.getElementById('c5').style.transition = '1s linear';
+
+	document.getElementById('c3').style.marginTop = '-637px';
+	document.getElementById('c3').style.opacity = '0.5';
+ }
+	function ac4(){
+	document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+	document.getElementById('c1').style.transition = '1s linear';
+	document.getElementById('c2').style.transition = '1s linear';
+	document.getElementById('c3').style.transition = '1s linear';
+	document.getElementById('c4').style.transition = '1s linear';
+	document.getElementById('c5').style.transition = '1s linear';
+
+	document.getElementById('c4').style.marginTop = '-637px'; 
+
+	document.getElementById('c4').style.opacity = '0.5';
+ }
+	function ac5(){
+	document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+	document.getElementById('c1').style.transition = '0.3s linear';
+	document.getElementById('c2').style.transition = '0.3s linear';
+	document.getElementById('c3').style.transition = '0.3s linear';
+	document.getElementById('c4').style.transition = '0.3s linear';
+	document.getElementById('c5').style.transition = '0.3s linear';
+
+	document.getElementById('c5').style.opacity = '0.5';
+
+		document.getElementById('c1').style.marginTop = '0px';
+		document.getElementById('c2').style.marginTop = '0px';
+		document.getElementById('c3').style.marginTop = '0px';
+		document.getElementById('c4').style.marginTop = '0px';
+		document.getElementById('c5').style.marginTop = '0px';
+
+	document.getElementById('c1').style.opacity = '1';
+	document.getElementById('c2').style.opacity = '1';
+	document.getElementById('c3').style.opacity = '1';
+	document.getElementById('c4').style.opacity = '1';
+	document.getElementById('c5').style.opacity = '1';
+
+	document.getElementById('flex1').style.cursor = 'pointer';
+	document.getElementById('flex2').style.cursor = 'pointer';
+	document.getElementById('flex3').style.cursor = 'pointer';
+	document.getElementById('flex4').style.cursor = 'pointer';
+	document.getElementById('flex5').style.cursor = 'pointer';
+	document.getElementById('flex2').style.cursor = 'pointer';
+	document.getElementById('flex3').style.cursor = 'pointer';
+	document.getElementById('flex4').style.cursor = 'pointer';
+	document.getElementById('flex5').style.cursor = 'pointer';
+	document.getElementById('flex6').style.cursor = 'pointer';
+	document.getElementById('flex7').style.cursor = 'pointer';
+	document.getElementById('flex8').style.cursor = 'pointer';
+	document.getElementById('flex9').style.cursor = 'pointer';
+	document.getElementById('flex10').style.cursor = 'pointer';
+	document.getElementById('flex11').style.cursor = 'pointer';
+	document.getElementById('flex12').style.cursor = 'pointer';
+	document.getElementById('flex13').style.cursor = 'pointer';
+	document.getElementById('flex14').style.cursor = 'pointer';
+	document.getElementById('flex15').style.cursor = 'pointer';
+	document.getElementById('flex16').style.cursor = 'pointer';
+	document.getElementById('flex17').style.cursor = 'pointer';
+	document.getElementById('flex18').style.cursor = 'pointer';
+	document.getElementById('flex19').style.cursor = 'pointer';
+	document.getElementById('flex20').style.cursor = 'pointer';
+	document.getElementById('flex21').style.cursor = 'pointer';
+	document.getElementById('flex22').style.cursor = 'pointer';
+	document.getElementById('flex23').style.cursor = 'pointer';
+	document.getElementById('flex24').style.cursor = 'pointer';
+	document.getElementById('flex25').style.cursor = 'pointer';
+
+	document.getElementById('flex2').style.visibility = 'visible';
+	document.getElementById('flex3').style.visibility = 'visible';
+	document.getElementById('flex4').style.visibility = 'visible';
+	document.getElementById('flex5').style.visibility = 'visible';
+	document.getElementById('flex6').style.visibility = 'visible';
+	document.getElementById('flex7').style.visibility = 'visible';
+	document.getElementById('flex8').style.visibility = 'visible';
+	document.getElementById('flex9').style.visibility = 'visible';
+	document.getElementById('flex10').style.visibility = 'visible';
+	document.getElementById('flex11').style.visibility = 'visible';
+	document.getElementById('flex12').style.visibility = 'visible';
+	document.getElementById('flex13').style.visibility = 'visible';
+	document.getElementById('flex14').style.visibility = 'visible';
+	document.getElementById('flex15').style.visibility = 'visible';
+	document.getElementById('flex16').style.visibility = 'visible';
+	document.getElementById('flex17').style.visibility = 'visible';
+	document.getElementById('flex18').style.visibility = 'visible';
+	document.getElementById('flex19').style.visibility = 'visible';
+	document.getElementById('flex20').style.visibility = 'visible';
+	document.getElementById('flex21').style.visibility = 'visible';
+	document.getElementById('flex22').style.visibility = 'visible';
+	document.getElementById('flex23').style.visibility = 'visible';
+	document.getElementById('flex24').style.visibility = 'visible';
+	document.getElementById('flex25').style.visibility = 'visible';
+
+	document.getElementById('colhidden1').style.visibility = 'hidden';
+	document.getElementById('colhidden2').style.visibility = 'hidden';
+	document.getElementById('colhidden3').style.visibility = 'hidden';
+	document.getElementById('colhidden4').style.visibility = 'hidden';
+	document.getElementById('colhidden5').style.visibility = 'hidden';
+	document.getElementById('colhidden6').style.visibility = 'hidden';
+	document.getElementById('colhidden7').style.visibility = 'hidden';
+	document.getElementById('colhidden8').style.visibility = 'hidden';
+	document.getElementById('colhidden9').style.visibility = 'hidden';
+	document.getElementById('colhidden10').style.visibility = 'hidden';
+// Se eu quiser que o scrool apareça ao subir a página, mas quando chegar em cima.
+/*	document.getElementsByTagName('body')[0].style.overflow = 'auto';
+	setTimeout(function schidden(){ document.getElementsByTagName('body')[0].style.overflow = 'hidden';},900)*/
+ }		
